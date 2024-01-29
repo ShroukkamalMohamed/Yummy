1aeddc2f34ac7e5687091aac629395eb66b9435d
shrouk kamal <shroukkamal15@gmail.com>
2024-01-29 23:48:51 +0200

    mealDetails

---
+++ JS/MealDetails.js
@@ -0,0 +1,12 @@
+// ?=====================>>>Elements===============================
+let Recipes = document.getElementById("Recipes");
+let Category = document.getElementById("Category");
+let area = document.getElementById("area");
+let mealInstructions = document.getElementById("mealInstructions");
+let mealTitle = document.getElementById("mealTitle");
+let mealImg = document.getElementById("mealImg");
+// ^=========================>>>function=========================== 
+ 
+// ~====================>>>>>Events======================================
+import { name } from "./site.js";
+ console.log(name);
--- JS/site.js
+++ JS/site.js
@@ -2,7 +2,7 @@
 let pageContent = document.getElementById("pageContent");
 // !=========================>>>>PageLoad==========================
 featchMealsData();
- 
+
 // ^=========================>>>function=========================== 
 function openSideMenu() {
     $(".sideMenu").animate({ "left": 0 }, 1000);
@@ -23,11 +23,11 @@ async function featchMealsData() {
    `;
     for (let index = 0; index < data.meals.length; index++) {
         cards += ` 
-                   <div class="col-md-3 mealcard" style="display: block">
-                    <img src="${data.meals[index].strMealThumb}"  alt="">
-                    <h5 class="text-center mealName"  >${data.meals[index].strMeal}</h5>
-                 </div>
-                `;
+        <div class="col-md-3 mealcard" onclick="mealDetails('${data.meals[index].idMeal}')"  style="display: block">
+            <img src="${data.meals[index].strMealThumb}"  alt="">
+            <h5 class="text-center mealName"  >${data.meals[index].strMeal}</h5>
+        </div>
+    `;
     }
     pageContent.innerHTML = cards += ` </div>
     </div>`;
@@ -49,6 +49,7 @@ $(".links ul li a").click(function () {
 
 });
 function openPage(url, targetId) {
+    console.log("hhhh");
     pageContent.innerHTML = ""
     $.ajax({
         url: url,
@@ -61,6 +62,52 @@ function openPage(url, targetId) {
             console.error('Error loading page:', error);
         }
     });
+}
+async function mealDetails(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    console.log(data);
+    pageContent.innerHTML = ` <div class="mealContent mt-4" id="mealContent">
+   <div class="container mt-5">
+     <div class="row">
+       <div class="col-md-4">
+         <img src=${data.meals[0].strMealThumb} id="mealImg" alt="" />
+         <span id="mealTitle" style="font-size: 30px">${data.meals[0].strMeal}</span>
+       </div>
+       <div class="col-md-8 d-flex flex-column">
+         <div id="mealInstructions"style="font-size: 15px">${data.meals[0].strInstructions}</div>
+      <div class="row mt-3">
+       <span id="area" style="font-size: 22px"> area: ${data.meals[0].strArea}</span>
+       <span id="Category" style="font-size: 22px">Category: ${data.meals[0].strCategory}</span>
+       <span id="Recipes" style="font-size: 22px"> Recipes:
+       <ul class=" strIngredient list-unstyled d-flex flex-wrap justify-content-between gap-2" style="font-size: 20px  ">
+       <li>${data.meals[0].strIngredient1}</li>
+       <li>${data.meals[0].strIngredient2}</li>
+       <li>${data.meals[0].strIngredient3}</li>
+       <li>${data.meals[0].strIngredient4}</li>
+       <li>${data.meals[0].strIngredient5}</li>
+       <li>${data.meals[0].strIngredient6}</li>
+       <li>${data.meals[0].strIngredient7}</li>
+       <li>${data.meals[0].strIngredient8}</li>
+       <li>${data.meals[0].strIngredient9}</li>
+       <li>${data.meals[0].strIngredient10}</li>
+       </ul>
+       </span>
+       <span id="Tags" style="font-size: 22px">Tags : 
+       <br>
+       <span> <a class="btn btn-success" href="${data.meals[0].strYoutube}" style="color: white;
+       text-decoration: none;
+       padding: 10px;"> Source</a></span>
+       <span> <a class="btn btn-danger" href="${data.meals[0].strYoutube}" style="color: white;
+       text-decoration: none;
+       padding: 10px;"> Youtube</a></span>
+       </span>
+       </div>
+       </div>
+      
+     </div>
+   </div>
+ </div>`
 }
 
 
